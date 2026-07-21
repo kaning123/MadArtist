3d68d7d4ab40c2f7ad69ba0c46e4ebd1b07e4b22
Kaning123 <kaning123official@163.com>
2026-07-21 14:52:58 +0800

    feat: 更新了 Tools.VoiceSplit 模块以适应 Tools.TTS 模块的需求

---
+++ Tools/VoiceSplit/__init__.py
@@ -0,0 +1,3 @@
+from .Main import (cut_and_save_voices, 
+                   detect_voice_segments, 
+                   __INNER_VERSION__)
--- Tools/VoiceSplit/cli_tool.py
+++ Tools/VoiceSplit/cli_tool.py
@@ -2,23 +2,20 @@ import librosa
 import soundfile as sf
 import numpy as np
 from pathlib import Path
-from . import file_lib as fl
+import file_lib as fl
 import argparse
 import json
 import os
 
-__INNER_VERSION__ = "Alpha_0.0.1_202607"
+__INNER_VERSION__ = "Alpha_0.0.1_202605"
 OUTPUT_DIR = fl.merge_dir_txt2(fl.get_my_dir(), "Output")
-CONFIG_DIR = fl.merge_dir_txt2(fl.get_my_dir(), "Config")
-with open(fl.merge_dir_txt2(CONFIG_DIR, "Main.json"), "r") as f:
-    config = json.load(f)
-    
+
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
     
 def detect_voice_segments(
     audio_path,
-    sr : int | None = 22050,
+    sr=22050,
     top_db=25,        # 低于这个分贝的视为静音，人声通常高于-25dB
     min_duration=0.1  # 人声片段最小持续时间（秒），过滤掉噪声
 ):
@@ -26,7 +23,7 @@ def detect_voice_segments(
     检测音频中的人声片段，返回(起始时间, 结束时间)列表
     """
     # 加载音频
-    y, sr = librosa.load(audio_path, sr=sr) # type: ignore
+    y, sr = librosa.load(audio_path, sr=sr)
     
     # 计算静音区间（返回静音的区间）
     non_silent_intervals = librosa.effects.split(y, top_db=top_db)
@@ -47,10 +44,9 @@ def detect_voice_segments(
 def cut_and_save_voices(
     audio_path,
     output_dir=fl.merge_dir_txt2(fl.get_my_dir(), "Output"),
-    sr : int | None = 22050,
+    sr=22050,
     top_db=25,
     min_duration=0.1,
-    excepted_results: int | None = None,
 ):
     """
     切割音频中的人声片段，保存到指定目录
@@ -69,10 +65,6 @@ def cut_and_save_voices(
         return []
     
     print(f"检测到 {len(segments)} 个人声片段")
-    if excepted_results is not None and len(segments) != excepted_results:
-        print(f"检测到的人声片段数量与预期不符，实际: {len(segments)}, 预期: {excepted_results}")
-        raise ValueError("检测到的人声片段数量与预期不符")
-        
     paths = []
     # 保存每个片段
     for i, (start_sec, end_sec) in enumerate(segments):
@@ -87,3 +79,16 @@ def cut_and_save_voices(
     
     return paths
 
+if __name__ == "__main__":
+    print(f"MadArtist VoiceSplit Module - {__INNER_VERSION__}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--audio_path", "-a", type=str, help="音频文件路径")
+    parser.add_argument("--output_dir", "-o", type=str, default=fl.merge_dir_txt2(fl.get_my_dir(), "Output"), help="输出目录")
+    parser.add_argument("--config_path", "-c", type=str, default=None, help="配置文件路径")
+    args = parser.parse_args()
+    if args.config_path is not None:
+        with open(args.config_path, "r") as f:
+            config = json.load(f)
+        cut_and_save_voices(args.audio_path, args.output_dir,**config)
+    else:
+        cut_and_save_voices(args.audio_path, args.output_dir)
